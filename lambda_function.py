940fddc882ac731a7747295cb91aa1632d570c7d
Heshan Vichalya Perera <heshanp.22@cse.mrt.ac.lk>
2025-02-20 16:36:53 +0530

    38

--- lambda_function.py
+++ lambda_function.py
@@ -12,7 +12,7 @@ def lambda_handler(event, context):
         
         # Create a response message
         if age:
-            message = f"Hello, {name}! THIS IS VERSION 37.You are {age} years old."
+            message = f"Hello, {name}! THIS IS VERSION 38.You are {age} years old."
         else:
             message = f"Hello, {name}!"
         
